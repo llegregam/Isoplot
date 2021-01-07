4829cc8ad48c47d148530b9fc7d87049fd5b825a
LoloPopoPy <legregam@insa-toulouse.fr>
2021-01-07 11:30:00 +0100

    Changed abspath to isoplot modules in conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -13,7 +13,7 @@
 
 import sys
 import os
-sys.path.insert(0, os.path.abspath('../isoplot'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
